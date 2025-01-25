551cb5b0bb84ca9171ca8185060491222067372c
chickenbuger <gksdidxornjs@naver.com>
2025-01-25 14:00:57 +0900

    Outline add

--- Assets/YHS_FOLDER/Script/Bottle.cs
+++ Assets/YHS_FOLDER/Script/Bottle.cs
@@ -5,8 +5,35 @@ using UnityEngine;
 public class Bottle : MonoBehaviour
 {
     public int team;
+    //Blue Red
+    public Material[] OutLineMaterials;
 
     private Rigidbody rb;
+
+    public void AttachmentOutline()
+    {
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+
+        if(meshRenderer)
+        {
+            Material[] sharedMaterials = meshRenderer.sharedMaterials;
+
+            // 새로운 배열 생성 (기존 배열 + 1 크기)
+            Material[] updatedMaterials = new Material[sharedMaterials.Length + 1];
+
+            // 기존 Material 배열 복사
+            for (int i = 0; i < sharedMaterials.Length; i++)
+            {
+                updatedMaterials[i] = sharedMaterials[i];
+            }
+
+            // 새 Material 추가
+            updatedMaterials[sharedMaterials.Length] = OutLineMaterials[team - 1];
+
+            // MeshRenderer의 Material 배열 업데이트
+            meshRenderer.sharedMaterials = updatedMaterials;
+        }
+    }
 
     public void AttackBottle(float InPower)
     {
